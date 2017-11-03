29c6e9d16681101eca6b6ec1b6613ee646e7d4fc
Shaanrs7 <srs383@cornell.edu>
2017-11-03 17:15:53 -0400

    Shaan: Optimized SW

--- lab4/ecelinux/layer.cpp
+++ lab4/ecelinux/layer.cpp
@@ -114,15 +114,77 @@ int i_index;
 // @param[out] : output - output fmaps
 
 void perform_dense (float* input, float*output, const float*weight, const float* bias, int M, int N) {
-
-  for (int n = 0; n < N; n++) {
-    for (int m = 0; m < M; m++) {
-      int w_index = m + n * M;
-      output[n] += input[m] * weight[w_index];
+int w_index;
+float biased;
+  for (int n = 0; n < N; n=n+4) {
+    for (int m = 0; m < M; m=m+2) {
+       w_index = m + n * M;
+      output[n] = output[n] + input[m] * weight[w_index] + input[m+1] * weight[w_index+1];// +  input[m+2] * weight[w_index+2] +  output[n] + input[m+3] * weight[w_index+3];
+      output[n+1] = output[n+1] + input[m] * weight[w_index+M] + input[m+1] * weight[w_index+M+1];// +  input[m+2] * weight[w_index+2] +  output[n] + input[m+3] * weight[w_index+3];
+      output[n+2] = output[n+2] + input[m] * weight[w_index+2*M] + input[m+1] * weight[w_index+2*M+1];// +  input[m+2] * weight[w_index+2] +  output[n] + input[m+3] * weight[w_index+3];
+      output[n+3] = output[n+3] + input[m] * weight[w_index+3*M] + input[m+1] * weight[w_index+3*M+1];// +  input[m+2] * weight[w_index+2] +  output[n] + input[m+3] * weight[w_index+3];
+/*      output[n+4] = output[n+4] + input[m] * weight[w_index+4*M] + input[m+1] * weight[w_index+4*M+1];// +  input[m+2] * weight[w_index+2] +  output[n] + input[m+3] * weight[w_index+3];
+      output[n+5] = output[n+5] + input[m] * weight[w_index+5*M] + input[m+1] * weight[w_index+5*M+1];// +  input[m+2] * weight[w_index+2] +  output[n] + input[m+3] * weight[w_index+3];
+      output[n+6] = output[n+6] + input[m] * weight[w_index+6*M] + input[m+1] * weight[w_index+6*M+1];// +  input[m+2] * weight[w_index+2] +  output[n] + input[m+3] * weight[w_index+3];
+      output[n+7] = output[n+7] + input[m] * weight[w_index+7*M] + input[m+1] * weight[w_index+7*M+1];// +  input[m+2] * weight[w_index+2] +  output[n] + input[m+3] * weight[w_index+3];
+      output[n+8] = output[n+8] + input[m] * weight[w_index+8*M] + input[m+1] * weight[w_index+8*M+1];// +  input[m+2] * weight[w_index+2] +  output[n] + input[m+3] * weight[w_index+3];
+      output[n+9] = output[n+9] + input[m] * weight[w_index+9*M] + input[m+1] * weight[w_index+9*M+1];// +  input[m+2] * weight[w_index+2] +  output[n] + input[m+3] * weight[w_index+3];
+      output[n+10] = output[n+10] + input[m] * weight[w_index+10*M] + input[m+1] * weight[w_index+10*M+1];// +  input[m+2] * weight[w_index+2] +  output[n] + input[m+3] * weight[w_index+3];
+      output[n+11] = output[n+11] + input[m] * weight[w_index+11*M] + input[m+1] * weight[w_index+11*M+1];// +  input[m+2] * weight[w_index+2] +  output[n] + input[m+3] * weight[w_index+3];
+      output[n+12] = output[n+12] + input[m] * weight[w_index+12*M] + input[m+1] * weight[w_index+12*M+1];// +  input[m+2] * weight[w_index+2] +  output[n] + input[m+3] * weight[w_index+3];
+      output[n+13] = output[n+13] + input[m] * weight[w_index+13*M] + input[m+1] * weight[w_index+13*M+1];// +  input[m+2] * weight[w_index+2] +  output[n] + input[m+3] * weight[w_index+3];
+      output[n+14] = output[n+14] + input[m] * weight[w_index+14*M] + input[m+1] * weight[w_index+14*M+1];// +  input[m+2] * weight[w_index+2] +  output[n] + input[m+3] * weight[w_index+3];
+      output[n+15] = output[n+15] + input[m] * weight[w_index+15*M] + input[m+1] * weight[w_index+15*M+1];// +  input[m+2] * weight[w_index+2] +  output[n] + input[m+3] * weight[w_index+3];
+*/
     }
-    float biased = output[n] + bias[n];
+     biased = output[n] + bias[n];
     output[n] = (biased > 0) ? biased : 0;
-  }
+    
+ biased = output[n+1] + bias[n+1];
+    output[n+1] = (biased > 0) ? biased : 0;
+
+     biased = output[n+2] + bias[n+2];
+    output[n+2] = (biased > 0) ? biased : 0;
+
+     biased = output[n+3] + bias[n+3];
+    output[n+3] = (biased > 0) ? biased : 0;
+/*
+     biased = output[n+4] + bias[n+4];
+    output[n+4] = (biased > 0) ? biased : 0;
+
+     biased = output[n+5] + bias[n+5];
+    output[n+5] = (biased > 0) ? biased : 0;
+
+     biased = output[n+6] + bias[n+6];
+    output[n+6] = (biased > 0) ? biased : 0;
+
+     biased = output[n+7] + bias[n+7];
+    output[n+7] = (biased > 0) ? biased : 0;
+
+     biased = output[n+8] + bias[n+8];
+    output[n+8] = (biased > 0) ? biased : 0;
+
+     biased = output[n+9] + bias[n+9];
+    output[n+9] = (biased > 0) ? biased : 0;
+
+     biased = output[n+10] + bias[n+10];
+    output[n+10] = (biased > 0) ? biased : 0;
+
+     biased = output[n+11] + bias[n+11];
+    output[n+11] = (biased > 0) ? biased : 0;
+
+     biased = output[n+12] + bias[n+12];
+    output[n+12] = (biased > 0) ? biased : 0;
+
+     biased = output[n+13] + bias[n+13];
+    output[n+13] = (biased > 0) ? biased : 0;
+
+     biased = output[n+14] + bias[n+14];
+    output[n+14] = (biased > 0) ? biased : 0;
+
+     biased = output[n+15] + bias[n+15];
+    output[n+15] = (biased > 0) ? biased : 0;
+ */ }
 
 }
 
